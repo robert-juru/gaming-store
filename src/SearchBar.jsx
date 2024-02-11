e45b34b15a1791ad83d3db9f03903a17704ac3ee
robert-juru <juru0188@gmail.com>
2024-02-11 21:04:44 +0200

    Added game page linking in search bar preview for improved navigation

--- src/SearchBar.jsx
+++ src/SearchBar.jsx
@@ -2,11 +2,16 @@ import { useState, useEffect, useMemo } from "react";
 import { useQuery } from "@tanstack/react-query";
 import debounce from "lodash.debounce";
 import { fetchSearchData } from "./Api";
-import { useNavigate, useLocation } from "react-router-dom";
+import { useNavigate, useLocation, Link } from "react-router-dom";
 import { IconContext } from "react-icons";
 import { CiSearch } from "react-icons/ci";
 
-const SearchBar = ({ displayedGames, setDisplayedGames, searchQueryData, setSearchQueryData }) => {
+const SearchBar = ({
+  displayedGames,
+  setDisplayedGames,
+  searchQueryData,
+  setSearchQueryData,
+}) => {
   const [query, setQuery] = useState("");
   const [searchResults, setSearchResults] = useState([]);
   const [searchedResultsHistory, setSearchedResultsHistory] = useState([]);
@@ -28,8 +33,6 @@ const SearchBar = ({ displayedGames, setDisplayedGames, searchQueryData, setSear
       setTimeout(() => {
         setDisplayedGames(searchQuery.data);
       }, 500);
-      
-
     }
   };
 
@@ -52,10 +55,12 @@ const SearchBar = ({ displayedGames, setDisplayedGames, searchQueryData, setSear
       console.log(searchQuery.data);
       setShouldFetch(false);
       setSearchQueryData((prevHistory) => {
-        const newGames = searchQuery.data.filter(game => !prevHistory.some(prevGame => prevGame.id === game.id));
+        const newGames = searchQuery.data.filter(
+          (game) => !prevHistory.some((prevGame) => prevGame.id === game.id),
+        );
         return [...prevHistory, ...newGames];
       });
-      console.log(searchQueryData)
+      console.log(searchQueryData);
     }
   }, [searchQuery.data, searchQuery.isSuccess, setSearchQueryData]);
 
@@ -90,18 +95,22 @@ const SearchBar = ({ displayedGames, setDisplayedGames, searchQueryData, setSear
         </div>
       </div>
       {searchQuery.data && isFocused && (
-        <div className=" scrollbar-thumb-slate-300 scrollbar-thin scrollbar-track-transparent absolute left-0 top-full z-50 max-h-60 w-full overflow-y-scroll rounded-md bg-slate-700">
+        <div className=" absolute left-0 top-full z-50 max-h-60 w-full overflow-y-scroll rounded-md bg-slate-700 scrollbar-thin scrollbar-track-transparent scrollbar-thumb-slate-300">
           {searchQuery.data.slice(0, 8).map((game) => (
-            <div
-              className="flex items-center gap-4 p-2 text-slate-300 hover:bg-slate-600 hover:text-white"
-              key={game.id}
-            >
-              <img
-                className="h-16 w-24 rounded-md"
-                src={game.background_image}
-                alt={`${game.background_image} image`}
-              />
-              <p>{game.name}</p>
+            <div key={game.id} onClick={(e) => e.stopPropagation()}>
+              <Link to={`/game/${game.id}`}>
+                <div
+                  className="border-1 flex items-center gap-4 border border-red-500 p-2 text-slate-300 hover:bg-slate-600 hover:text-white"
+                  onMouseDown={(e) => e.preventDefault()}
+                >
+                  <img
+                    className="h-16 w-24 rounded-md"
+                    src={game.background_image}
+                    alt={`${game.background_image} image`}
+                  />
+                  <p>{game.name}</p>
+                </div>
+              </Link>
             </div>
           ))}
         </div>
